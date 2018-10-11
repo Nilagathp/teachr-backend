23347a1c96243e0b4a5f1b5607db7e2e3273223e
jensnyder <jlsnyder09@gmail.com>
2018-10-11 12:00:03 -0400

    added auth, teachers can log in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
   def create
     @user = User.create(user_params)
     if @user.valid?
+      @token = encode_token(user_id: @user.id)
       render json: {user: UserSerializer.new(@user)}, status: :created
     else
       render json: {error: 'failed to create user'}, status: :not_acceptable
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :teachers, only: [:index]
   resources :users, only: [:create]
-  # post '/login', to 'auth#create'
+  post '/login', to: 'auth#create'
 end

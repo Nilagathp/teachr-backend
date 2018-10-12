fe8829cfcdf2fee20cede5551b4a9c78130377b1
jensnyder <jlsnyder09@gmail.com>
2018-10-12 11:34:47 -0400

    added profile route

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :teachers, only: [:index]
   resources :users, only: [:create]
   post '/login', to: 'auth#create'
+  get '/profile', to: 'users#profile'
 end

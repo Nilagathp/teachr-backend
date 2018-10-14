02705435762ce98425c0e7248555042568efd1da
jensnyder <jlsnyder09@gmail.com>
2018-10-14 18:31:35 -0400

    Added route/controller to delete assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,18 +1,29 @@
 class AssignmentsController < ApplicationController
 
+  before_action :find_assignment, only [:update, :destroy]
+
   def create
     @assignment = Assignment.create(assignment_params)
     render json: @assignment
   end
 
   def update
-    @assignment = Assignment.find(params[:id])
     @assignment.update(assignment_params)
     render json: @assignment
   end
 
+  def destroy
+    render json: @assignment.destroy
+  end
+
   private
+
   def assignment_params
     params.require(:assignment).permit(:name, :course_id, :category, :points)
   end
+
+  def find_assignment
+    @assignment = Assignment.find(params[:id])
+  end
+  
 end

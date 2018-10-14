56924e08af14c2a614f3aeb2527e5c43b4b11edc
jensnyder <jlsnyder09@gmail.com>
2018-10-14 18:25:54 -0400

    Added route and controller to update assignment

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -5,6 +5,12 @@ class AssignmentsController < ApplicationController
     render json: @assignment
   end
 
+  def update
+    @assignment = Assignment.find(params[:id])
+    @assignment.update(assignment_params)
+    render json: @assignment
+  end
+
   private
   def assignment_params
     params.require(:assignment).permit(:name, :course_id, :category, :points)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:create]
-  resources :assignments, only: [:create]
+  resources :assignments, only: [:create, :update]
   post '/login', to: 'auth#create'
   get '/profile', to: 'users#profile'
 end

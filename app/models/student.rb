4a1efa0c8a82cb55d45f381d28f1a25807d0616b
jensnyder <jlsnyder09@gmail.com>
2018-10-15 13:18:37 -0400

    Added assignments to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,4 +3,5 @@ class Student < ApplicationRecord
   has_many :student_sections
   has_many :sections, through: :student_sections
   has_many :courses, through: :sections
+  has_many :assignments, through: :courses
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,4 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :name, :id, :grade, :sections, :courses
+  attributes :name, :id, :grade, :sections, :courses, :assignments
   has_many :student_sections
 end

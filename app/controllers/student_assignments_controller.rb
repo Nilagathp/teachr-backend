8636221e41ed6da89070a6dd1556af0e98be5ede
jensnyder <jlsnyder09@gmail.com>
2018-10-15 17:29:45 -0400

    Added update route and params to StudentAssignment

--- app/controllers/student_assignments_controller.rb
+++ app/controllers/student_assignments_controller.rb
@@ -4,9 +4,15 @@ class StudentAssignmentsController < ApplicationController
     render json: @student_assignment
   end
 
+  def update
+    @student_assignment = StudentAssignment.find(params[:id])
+    @student_assignment.update(student_assignment_params)
+    render json: @student_assignment
+  end
+
   private
   def student_assignment_params
-    params.require(:student_assignment).permit(:student_id, :assignment_id, :points_earned, :answers, :status)
+    params.require(:student_assignment).permit(:student_id, :assignment_id, :points_earned, :status, :answers => [])
   end
 
 end

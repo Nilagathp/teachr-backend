06d9aa04a3b5d5df275e59d013fba8e6334aafb0
jensnyder <jlsnyder09@gmail.com>
2018-10-16 13:46:07 -0400

    updated student serializer and assignment model

--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,4 +1,4 @@
 class StudentSerializer < ActiveModel::Serializer
-  attributes :name, :id, :grade, :sections, :student_assignments, :courses
+  attributes :name, :id, :grade, :sections, :assignments, :student_assignments, :courses
   # has_many :sections, through: :student_sections
 end

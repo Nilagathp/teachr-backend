a892fe75b1516a3ad808d6d1383703e3624523db
jensnyder <jlsnyder09@gmail.com>
2018-10-21 20:20:20 -0400

    Added due_date to assignments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_172405) do
+ActiveRecord::Schema.define(version: 2018_10_22_001706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_10_21_172405) do
     t.datetime "updated_at", null: false
     t.text "directions"
     t.jsonb "content"
+    t.datetime "due_date"
   end
 
   create_table "courses", force: :cascade do |t|
